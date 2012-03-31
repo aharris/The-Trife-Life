60c7bccffa68f096997bdcf7ea62c7a13640c6d4
Ashton Harris <ahdesignsonline@gmail.com>
2012-03-31 13:35:30 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.1'
+gem 'thin'
+gem 'heroku'
+gem 'json'
+gem 'bourbon'
+gem 'jquery-rails'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.1.4'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :production do
+  gem 'pg'
+end
+
+group :development do
+  gem 'foreman'
+  gem 'sqlite3', '1.3.4'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thetrifelife::Application.config.secret_token = '3e324e2fb6c5588178e077187db87ff8759257bfd3de33cfe410bee193d90c6b04585e392f3f7871e5b5747d161ad90292abf93637f2ea958a078f6b9764b79c'
